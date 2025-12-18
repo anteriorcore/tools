473c03bc9a9f63091a7ae1be34234aa0a26344f2
Robin <robin.lewis@anterior.com>
2025-12-17 19:53:36 -0500

    feat: nix-flake-check-changed and treefmt

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    systems.url = "systems";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    { self, flake-parts, ... }@inputs:
+    let
+      checkBuildAll = import ./nix/check-build-all.nix;
+      allSystems = {
+        flake.flakeModules = { inherit checkBuildAll; };
+        perSystem =
+          { pkgs, lib, ... }:
+          {
+            packages =
+              let
+                all = lib.packagesFromDirectoryRecursive {
+                  inherit (pkgs) callPackage newScope;
+                  directory = ./packages;
+                };
+              in
+              {
+                inherit (all) nix-flake-check-changed;
+              };
+            treefmt = import ./nix/treefmt.nix;
+          };
+      };
+    in
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+      imports = [
+        inputs.treefmt-nix.flakeModule
+        allSystems
+        checkBuildAll
+      ];
+    };
+}
