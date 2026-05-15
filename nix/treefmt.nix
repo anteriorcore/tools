e3595c5561e0518318960571521d416dd9d36705
BenZuckier <ben@anterior.com>
2026-05-15 16:00:06 -0400

    feat: yaml format

--- nix/treefmt.nix
+++ nix/treefmt.nix
@@ -1,9 +1,14 @@
 { ... }:
 {
   projectRootFile = "flake.nix";
-  programs.nixfmt = {
-    enable = true;
-    strict = true;
+  programs = {
+    # keep-sorted start block=true
+    keep-sorted.enable = true;
+    nixfmt = {
+      enable = true;
+      strict = true;
+    };
+    yamlfmt.enable = true;
+    # keep-sorted end
   };
-  programs.keep-sorted.enable = true;
 }
