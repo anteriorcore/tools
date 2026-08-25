129dae0c66c574a56ea59aefd08a3300d38ee97f
Robin <robin.lewis@anterior.com>
2026-08-24 20:29:07 -0400

    build: remove x86 patch (vendor works)

--- packages/npm-overrides/package.nix
+++ packages/npm-overrides/package.nix
@@ -4,13 +4,11 @@ final: prev: {
   "node_modules/tree-sitter-python" = prev."node_modules/tree-sitter-python".overrideAttrs {
     patchPhase = ''
       cp -f ${./tree-sitter-prebuilds/linux-arm64/tree-sitter-python.node} ./prebuilds/linux-arm64/tree-sitter-python.node
-      cp -f ${./tree-sitter-prebuilds/linux-x64/tree-sitter-python.node} ./prebuilds/linux-x64/tree-sitter-python.node
     '';
   };
   "node_modules/tree-sitter-typescript" = prev."node_modules/tree-sitter-typescript".overrideAttrs {
     patchPhase = ''
       cp -f ${./tree-sitter-prebuilds/linux-arm64/tree-sitter-typescript.node} ./prebuilds/linux-arm64/tree-sitter-typescript.node
-      cp -f ${./tree-sitter-prebuilds/linux-x64/tree-sitter-typescript.node} ./prebuilds/linux-x64/tree-sitter-typescript.node
     '';
   };
 }
